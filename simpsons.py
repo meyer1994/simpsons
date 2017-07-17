581780880b30139e4515cfa8a733d3842c5616cc
Joao Meyer <1994meyer@gmail.com>
2017-07-17 18:02:19 -0300

    a little bit of documentation

--- simpsons.py
+++ simpsons.py
@@ -5,20 +5,58 @@ import re
 
 from bs4 import BeautifulSoup
 
+# base website url
 WEBSITE = 'http://www.springfieldspringfield.co.uk/'
 
 def get_words(text):
+    '''
+    Get words.
+
+    Function created to get all the words from a string. It uses regex from
+    this StackOverflow question:
+        https://stackoverflow.com/questions/12705293/regex-to-split-words-in-python/12705513
+
+    Args:
+        text: An string to get the words from.
+
+    Returns:
+        A list with all the words from the string.
+    '''
+
     regex = re.compile("(\w[\w']*\w|\w)")
     text = text.lower()
     return regex.findall(text)
 
 
 def get_episodes_list():
+    '''
+    Get episodes list.
+
+    A simple function that will return all the episodes' script urls from the
+    website.
+
+    Returns:
+        List with urls for the scripts.
+    '''
+
     r = requests.get('http://www.springfieldspringfield.co.uk/episode_scripts.php?tv-show=the-simpsons')
     bs = BeautifulSoup(r.text, 'html.parser')
     return [i['href'] for i in bs.find_all('a', class_='season-episode-title')]
 
+
 def get_script(episode_url):
+    '''
+    Get script.
+
+    Gets the scripts, as strings, from the website.
+
+    Args:
+        episode_url: Url of where to get the script from.
+
+    Returns:
+        String, unedited, containing the script.
+    '''
+
     r = requests.get(episode_url)
     bs = BeautifulSoup(r.text, 'html.parser')
     s = bs.find('div', class_='episode_script')
@@ -38,6 +76,7 @@ for url in episodes_urls:
     words = get_words(script)
     ep_count = dict()
 
+    # count words
     for w in words:
         try:
             word_count[w] += 1
