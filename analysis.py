7db123ccbc4ef1d64e6a17cfe88369d3746c3c21
Joao Meyer <1994meyer@gmail.com>
2017-07-17 22:55:48 -0300

    added, by request, a filtered version of the list. with some words filtered

---
+++ analysis.py
@@ -0,0 +1,20 @@
+import csv
+
+# get stop words
+# list copied from here: http://www.lextek.com/manuals/onix/stopwords1.html
+with open('stopwords.txt', 'r') as stop_file:
+    stop_words = stop_file.read()
+    stop_words = stop_words.split('\n')
+
+# get all words
+with open('total_count.csv', 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    mydict = dict(reader)
+
+# write to new csv
+with open('final_stop.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in mydict.items():
+        # filter
+        if key not in stop_words:
+            writer.writerow([key, value])
